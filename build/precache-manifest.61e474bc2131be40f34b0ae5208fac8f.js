fef81367ec8e644399ebff0ba53468bfd65bd456
sara-bahr <37916820+sara-bahr@users.noreply.github.com>
2020-06-05 08:20:21 +1200

    adding build directory

---
+++ build/precache-manifest.61e474bc2131be40f34b0ae5208fac8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db3e3f8fcf57147f63c957b8e8a4a31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce824a15ba2c84e958e6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "649cf2a6687c1aeb4249",
+    "url": "/static/css/main.6ef28d60.chunk.css"
+  },
+  {
+    "revision": "ce824a15ba2c84e958e6",
+    "url": "/static/js/2.83f44c3e.chunk.js"
+  },
+  {
+    "revision": "168a437c30bacdc761ffacabfe4a8c66",
+    "url": "/static/js/2.83f44c3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "649cf2a6687c1aeb4249",
+    "url": "/static/js/main.72fe2143.chunk.js"
+  },
+  {
+    "revision": "b143f9b433f148ec6ba4",
+    "url": "/static/js/runtime-main.23aa413f.js"
+  }
+]);
